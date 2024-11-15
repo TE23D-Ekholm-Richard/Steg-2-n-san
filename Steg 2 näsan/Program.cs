44a5d4f78bd1cb5039b421af15f7fb3852088a2a
Richard Ekholm <richard.ekholm@elev.ga.ntig.se>
2024-11-15 09:59:06 +0100

    signma

---
+++ Steg 2 näsan/Program.cs
@@ -0,0 +1,14 @@
+﻿// List<string> leksaker = ["Vatten pistol", "Dinosaurie", "Beyblade", "Bakugan", "Pokemon kort"];
+
+// Console.WriteLine(leksaker[1]);
+
+// for(int i = 0 ; i<5; i++)
+// {
+//     Console.WriteLine(leksaker[i]);
+// }
+
+// List<string> kamrater = ["Kevin", " Simon", "Konstapeltin", "Luddebozz", "Theo"];
+
+// List<int> i = [2, 4, 7, 9, 1];
+
+Console.ReadLine();
