9f24d7bddbd5165cbbdd3a26abd905ac3977254e
Richard Ekholm <richard.ekholm@elev.ga.ntig.se>
2024-11-18 09:23:53 +0100

    klar nu

--- Steg 2 näsan/Program.cs
+++ Steg 2 näsan/Program.cs
@@ -1,14 +1,50 @@
-﻿// List<string> leksaker = ["Vatten pistol", "Dinosaurie", "Beyblade", "Bakugan", "Pokemon kort"];
+﻿//  List<string> leksaker = ["Vatten pistol", "Dinosaurie", "Beyblade", "Bakugan", "Pokemon kort"];
 
-// Console.WriteLine(leksaker[1]);
+//  Console.WriteLine(leksaker[1]);
 
-// for(int i = 0 ; i<5; i++)
-// {
-//     Console.WriteLine(leksaker[i]);
-// }
+//  for(int i = 0 ; i<5; i++)
+//  {
+//      Console.WriteLine(leksaker[i]);
+//  }
 
+//  List<string> kamrater = ["Kevin", " Simon", "Konstapeltin", "Luddebozz", "Theo"];
+
+//  List<int> i = [2, 4, 7, 9, 1];
+
+//  List<string> leksaker = ["Vatten pistol", "Dinosaurie", "Beyblade", "Bakugan", "Pokemon kort"];
 // List<string> kamrater = ["Kevin", " Simon", "Konstapeltin", "Luddebozz", "Theo"];
 
-// List<int> i = [2, 4, 7, 9, 1];
+//  List<int> a = [2, 4, 7, 9, 1];
 
-Console.ReadLine();
+//  for(int i = 0 ; i<5; i++)
+//  {
+//     Console.WriteLine($"{kamrater[i]} ger {leksaker[i]} betyget {a[i]}");
+//  }
+
+
+
+
+// List<string> cites = new List<string>();
+// while (true)
+// {
+//     Console.WriteLine("Skriv vad som helst för att lägga in i listan.");
+//     Console.WriteLine("Skriv exit och sedan enter för att avsluta");
+
+//     string a = Console.ReadLine();
+    
+//     if (a == "exit")
+//     {
+//         break;
+//     }
+//     cites.Add(a);
+
+// }
+
+// foreach (string city in cites)
+// {
+//     Console.WriteLine(city);
+// }
+
+
+
+// Console.ReadLine();
